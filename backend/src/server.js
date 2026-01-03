6a01508945cd4b0c9e52f99ccb8be3bef5fae83f
joelchirwa <joelchirwa@my.uopeople.edu>
2026-01-03 23:35:00 +0200

    first commit

---
+++ backend/src/server.js
@@ -0,0 +1,24 @@
+import express from 'express'
+import path from 'path';
+import ENV from './config.js';
+
+const app = express();
+
+const __dirname = path.resolve();
+
+app.get('/api/health', (req, res) => {
+    res.status(200).json({ message: 'OK' });
+});
+
+// making app ready for deployment
+if (ENV.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../admin/dist')));
+
+    app.get("/{*any}"), (req, res) => { 
+        res.sendFile(path.resolve(__dirname, '../admin', '/dist', 'index.html'));
+    }
+}
+
+app.listen(ENV.PORT, () => {
+    console.log(`Server is running on port ${ENV.PORT}`);
+    })
